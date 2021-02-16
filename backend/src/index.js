4b96b5e8499da367faa7758f53ab44d2c173c7b6
Rafael Viana <rafael.viana@ksgestao.com.br>
2021-02-16 16:42:04 -0300

    PRIMEIRO COMMIT

---
+++ backend/src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+mongoose.connect(
+  "mongodb+srv://USER:PASSWORD@cluster0-exuo2.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  next(); //executar o q vem dps
+});
+
+app.use(cors());
+
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
+
+app.use(require("./routes"));
+
+server.listen(3333);
